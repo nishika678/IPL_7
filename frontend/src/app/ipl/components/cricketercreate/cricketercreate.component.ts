8ce11afdf9a4909bfd4572adbd85c8c2eb1c298a
username <wecp-dummy@gmail.com>
2025-10-15 09:28:50 +0000

    wecp initial commit

---
+++ frontend/src/app/ipl/components/cricketercreate/cricketercreate.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-cricketer-create',
+    templateUrl:'cricketercreate.component.html',
+    styleUrls:['./cricketercreate.component.css']
+})
+export class CricketerCreateComponent {
+
+}
